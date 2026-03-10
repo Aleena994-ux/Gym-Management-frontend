b5c5baa05f4f02b40575b8d30e96de2057d540c6
Aleena <aleenaks9747@example.com>
2026-03-10 15:11:28 +0530

    updated

--- src/services/serverURL.js
+++ src/services/serverURL.js
@@ -1,3 +1,7 @@
-const SERVERURL = "http://localhost:3000"
+// const SERVERURL = "http://localhost:3000"
+
+const SERVERURL = "https://gym-management-backend-h0at.onrender.com"
+
+
 
 export default SERVERURL
